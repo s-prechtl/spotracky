6023093f342ca9b10c4cffb2b0e69f35d8da09e8
s-prechtl <stefan@tague.at>
2024-09-07 00:49:58 +0200

    feat: worky?

---
+++ updater.py
@@ -0,0 +1,20 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = psycopg2.connect(
+    host=os.getenv('DB_HOST'),
+    database=os.getenv('DB_NAME'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    port=os.getenv('DB_PORT')
+)
+
+cursor = conn.cursor()
+
+cursor.execute("UPDATE users SET count=count+1")
+cursor.close()
+conn.close()
+
